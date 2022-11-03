4f9686b55728b62b4d9222b5d5c856ac09be7954
lzp <1245491965@qq.com>
2022-11-03 17:44:08 +0800

    可以支持文本了

--- src/main/java/com/kedacom/test/test.java
+++ src/main/java/com/kedacom/test/test.java
@@ -1,13 +1,20 @@
 package com.kedacom.test;
 
 import com.kedacom.util.CustomXWPFDocument;
+import com.kedacom.util.DocUtilv2;
 import lombok.SneakyThrows;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,55 +25,69 @@ import java.util.regex.Pattern;
  * @apiNote : TODO
  */
 public class test {
-    @SneakyThrows
-    public static void main(String[] args) {
-        String path="C:\\Users\\lzp\\Desktop\\doc测试\\test.docx";
-        File file = new File(path);
-        if (!file.exists()){
-            File file1 = new File(file.getParent());
-            file1.mkdirs();
-            file.createNewFile();
+    public static void main(String[] args)  {
+        String path="C:\\Users\\lzp\\Desktop\\doc测试\\多个复选框同一行.docx";
+        Map<String, Object> param=new HashMap<>();
+        param.put("${JDLX}","--成功替换!--");
+        String s ="C:\\Users\\lzp\\Desktop\\doc测试\\多个复选框同一行" + (new Date()).getTime() + ".docx" ;
+        File file = new File(s);
+        try (FileOutputStream fos = new FileOutputStream(file);
+             CustomXWPFDocument document = new CustomXWPFDocument(new FileInputStream(path))) {
+
+            List<XWPFParagraph> paragraphs = document.getParagraphs();
+            for (XWPFParagraph paragraph : paragraphs) {
+                List<XWPFRun> runs = paragraph.getRuns();
+                Map<Integer,String> idForRuns=new HashMap<>(256);
+                //文本缓存,与id对应
+                for (int i = 0; i < runs.size(); i++) {
+                    idForRuns.put(i,runs.get(i).toString());
+                }
+                for (int i = 0; i < runs.size(); i++) {
+                    //获取本段run的文本
+                    String text = idForRuns.get(i);
+                    String newText = text;
+                    String reg="\\$\\{\\S+}";
+                    Pattern compile = Pattern.compile(reg);
+                    for (Map.Entry<String, Object> entry : param.entrySet()) {
+                        //匹配文本key
+                        Matcher matcher = compile.matcher(entry.getKey());
+                        while (matcher.find()){
+                            newText= newText.replace(entry.getKey(),entry.getValue().toString());
+                        }
+                    }
+                    XWPFRun xwpfRun = runs.get(i);
+                    xwpfRun.setText(newText,0);
+                    //paragraph.removeRun(i);
+                    //XWPFRun xwpfRun1 = paragraph.insertNewRun(i);
+                    //xwpfRun1.setText(newText);
+                }
+            }
+            document.write(fos);
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        FileInputStream inputStream = new FileInputStream(path);
-        CustomXWPFDocument customXWPFDocument = new CustomXWPFDocument(inputStream);
-        List<XWPFParagraph> paragraphs = customXWPFDocument.getParagraphs();
-        XWPFParagraph xwpfParagraph = paragraphs.get(0);
-
-        XWPFRun run = xwpfParagraph.createRun();
-        run.setFontFamily("Wingdings 2");
-        run.setText("\u0052 测试");
-        xwpfParagraph.addRun(run);
-
     }
 }
 class test2{
     public static void main(String[] args) {
-        String str="这是鉴定用途:${JDYT2}    第二次${JDYT2}";
-        int i = str.indexOf("${", -1);
-
-        char c = str.charAt(7);
-        System.out.println(c);
-        System.out.println(i);
-        String x = str.replace("${JDYT2}", "行政");
-        System.out.println(x);
+        String path="C:\\Users\\lzp\\Desktop\\doc测试\\多个复选框同一行.docx";
+        Map<String, Object> param=new HashMap<>();
+        param.put("JDLX","--成功替换!--");
+        DocUtilv2.replaceWordCode(param,path);
     }
 }
 class test3{
     public static void main(String[] args) {
 
-        String str="这是鉴定用途:${JDYT}  这是第二次:${JDYT} 这是另一个${JD}  第三次残缺的${";
+        String str="这是鉴定用途:${JYT}  这是第二次:${JDT} 这是另一个${JD}  第三次残缺的${";
         String reg="\\$\\{[A-Z]+}";
         Pattern compile = Pattern.compile(reg);
         Matcher matcher = compile.matcher(str);
-        if (matcher.find()) {
-            String group1 = matcher.group();
-            System.out.println(group1);
-            for (int i = 0; i < matcher.groupCount(); i++) {
-                int start = matcher.start(i);
-                String group = matcher.group(i);
-                int end = matcher.end(i);
-                System.out.println(start+"---"+group+"---"+end);
-            }
+        while (matcher.find()) {
+            int start = matcher.start();
+            String group = matcher.group();
+            int end = matcher.end();
+            System.out.println(start+"---"+group+"---"+end);
         }
     }
 }
--- src/main/java/com/kedacom/util/DocUtilv3.java
+++ src/main/java/com/kedacom/util/DocUtilv3.java
@@ -8,10 +8,7 @@ import org.springframework.util.ObjectUtils;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -35,7 +32,7 @@ public class DocUtilv3 {
                 //处理item系列编码
                 Map<String, Object> itemParam = handItemCodes(param);
                 for (XWPFParagraph paragraph : paragraphs) {
-                    paragraphHandler(paragraph, param,itemParam);
+                    paragraphHandler(paragraph, param);
                 }
             }
             //处理表格
@@ -46,7 +43,6 @@ public class DocUtilv3 {
                     List<XWPFTableCell> tableCells = row.getTableCells();
                     for (XWPFTableCell tableCell : tableCells) {
                         List<XWPFParagraph> tableCellParagraphs = tableCell.getParagraphs();
-                        //todo 和表格一样的处理流程,只处理文本/图片
                         for (XWPFParagraph paragraph : tableCellParagraphs) {
                             paragraphHandler(paragraph, param);
                         }
@@ -57,46 +53,128 @@ public class DocUtilv3 {
             List<XWPFHeader> headerList = document.getHeaderList();
             for (XWPFHeader xwpfHeader : headerList) {
                 List<XWPFParagraph> xwpfHeaderParagraphs = xwpfHeader.getParagraphs();
-                //todo 只处理文本
+                for (XWPFParagraph paragraph : xwpfHeaderParagraphs) {
+                    paragraphHandler(paragraph, param);
+                }
             }
             //页脚
             List<XWPFFooter> footerList = document.getFooterList();
             for (XWPFFooter xwpfFooter : footerList) {
                 List<XWPFParagraph> xwpfFooterParagraphs = xwpfFooter.getParagraphs();
-                //todo 只处理文本
+                for (XWPFParagraph paragraph : xwpfFooterParagraphs) {
+                    paragraphHandler(paragraph, param);
+                }
             }
+            document.write(fos);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
 
-    private static void paragraphHandler(XWPFParagraph paragraph, Map<String, Object> param, Map<String, Object> itemParam) {
-
-    }
-
     private static void paragraphHandler(XWPFParagraph paragraph, Map<String, Object> param) {
-        int startRunIndex=-1;
-        int endRunIndex=-1;
         //获取所有runs去掉空格拼接成整段文本(一个段落)
         List<XWPFRun> runs = paragraph.getRuns();
+        Map<Integer, String> idForRuns = new HashMap<>(256);
+        //文本缓存,与id对应
         for (int i = 0; i < runs.size(); i++) {
-            XWPFRun xwpfRun = runs.get(i);
-            String text = xwpfRun.text();
-            String reg="\\$\\{[A-Z]+}";
+            idForRuns.put(i, runs.get(i).toString());
+        }
+        //跨行文本预处理
+        multilineCodeHandler(paragraph, runs, idForRuns);
+        //文本编码处理
+        textCodeHandler(param, runs, idForRuns);
+        //todo 勾选框处理
+
+        //todo 图片编码处理
+    }
+
+    private static void textCodeHandler(Map<String, Object> param, List<XWPFRun> runs, Map<Integer, String> idForRuns) {
+        for (int i = 0; i < runs.size(); i++) {
+            //获取本段run的文本
+            String text = idForRuns.get(i);
+            String newText = text;
+            String reg = "\\$\\{\\w+}";
             Pattern compile = Pattern.compile(reg);
             for (Map.Entry<String, Object> entry : param.entrySet()) {
+                //匹配文本key
                 Matcher matcher = compile.matcher(entry.getKey());
-                if (matcher.find()){
-                    int end = matcher.end();
-                    text= text.replace(entry.getKey(),entry.getValue().toString());
+                while (matcher.find()) {
+                    newText = newText.replace(entry.getKey(), entry.getValue().toString());
                 }
+            }
+            XWPFRun xwpfRun = runs.get(i);
+            xwpfRun.setText(newText,0);
+        }
+    }
+
+    private static void multilineCodeHandler(XWPFParagraph paragraph, List<XWPFRun> runs, Map<Integer, String> idForRuns) {
+        int startRunIndex = -1;
+        int endRunIndex = -1;
+        for (int i = 0; i < runs.size(); i++) {
+            //获取本段run的文本
+            String text = idForRuns.get(i);
+            //匹配残缺的编码标识
+            String endRunMutilatedReg = "\\$\\{?\\S*$";
+            Pattern compile = Pattern.compile(endRunMutilatedReg);
+            Matcher matcher = compile.matcher(text);
+            if (matcher.find()) {
+                //记录残缺编码起始位置
+                startRunIndex = i;
+            }
+            int lastRunSubIndex = -1;
+            //寻找下一段匹配的编码末尾
+            if (startRunIndex != -1) {
+                String endRunReg = "^\\{?\\S*}";
+                Pattern pattern = Pattern.compile(endRunReg);
+                Matcher matcher1 = pattern.matcher(text);
+                if (matcher1.find()) {
+                    //记录末尾位置与字符下标
+                    endRunIndex = i;
+                    lastRunSubIndex = matcher1.end();
+                }
+            }
+            //处理跨行文本
+            if (startRunIndex != -1 && endRunIndex != -1) {
+                StringBuilder startRunText = new StringBuilder();
+                StringBuilder endRunText = new StringBuilder();
+                //处理文本
+                //todo 将处理后的文本与原本的样式原样设置到run,删除中间的run,更新run头和run尾 注意:移除run方法,后面的run会补位到前面
+                XWPFRun startRun = runs.get(startRunIndex);
+                XWPFRun endRun = runs.get(endRunIndex);
+                for (int index = startRunIndex; index <= endRunIndex; index++) {
+                    //中间的run属于编码部分
+                    if (index < endRunIndex) {
+                        startRunText.append(idForRuns.get(index));
+                    }
+                    //末尾位置需要区分编码部分与正常文本
+                    if (index == endRunIndex) {
+                        String lastRunText = idForRuns.get(index);
+                        String substrBefore = lastRunText.substring(lastRunSubIndex);
+                        startRunText.append(substrBefore);
+                        String substrAfter = lastRunText.replace(substrBefore, "");
+                        endRunText.append(substrAfter);
+                    }
+                    //删除中间的run和缓存的文本
+                    if (index > startRunIndex && index < endRunIndex) {
+                        paragraph.removeRun(index);
+                        idForRuns.remove(index);
+                    }
+                }
+                //pos代表w:t标签的下标
+                startRun.setText(startRunText.toString(), 0);
+                endRun.setText(endRunText.toString(), 0);
+                //重置标志位
+                startRunIndex = -1;
+                endRunIndex = -1;
             }
         }
     }
 
     private static String strHandler(String paragraphText, Map<String, Object> param) {
-        String text=textHandler(paragraphText,param);
-        text= imgHandler(text,param);
-        text= choolseHandler(text,param);
+        String text = textHandler(paragraphText, param);
+        text = imgHandler(text, param);
+        text = choolseHandler(text, param);
         return text;
     }
 
@@ -108,22 +186,24 @@ public class DocUtilv3 {
         return null;
     }
 
-    /** 普通文本编码处理
+    /**
+     * 普通文本编码处理
+     *
      * @param paragraphText
      * @param param
      * @return
      */
     private static String textHandler(String paragraphText, Map<String, Object> param) {
-        log.info("文本编码处理开始:{}",paragraphText);
-        String reg="\\$\\{[A-Z]+}";
+        log.info("文本编码处理开始:{}", paragraphText);
+        String reg = "\\$\\{[A-Z]+}";
         Pattern compile = Pattern.compile(reg);
         for (Map.Entry<String, Object> entry : param.entrySet()) {
             Matcher matcher = compile.matcher(entry.getKey());
-            if (matcher.find()){
-                paragraphText= paragraphText.replace(entry.getKey(),entry.getValue().toString());
+            if (matcher.find()) {
+                paragraphText = paragraphText.replace(entry.getKey(), entry.getValue().toString());
             }
         }
-        log.info("文本编码处理结束:{}",paragraphText);
+        log.info("文本编码处理结束:{}", paragraphText);
         return paragraphText;
     }
 
